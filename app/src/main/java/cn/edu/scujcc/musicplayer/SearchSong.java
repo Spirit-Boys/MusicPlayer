f149a8d77fc3464cda0c307d2c24e5adb6f6fc33
Ghy <63713953+Spirit-Boys@users.noreply.github.com>
2020-06-02 10:45:27 +0800

    初始化项目

---
+++ app/src/main/java/cn/edu/scujcc/musicplayer/SearchSong.java
@@ -0,0 +1,14 @@
+package cn.edu.scujcc.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchSong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_song);
+    }
+}
